b822b66853cc8d7f3f13785bd5d50b1c4cd1bda2
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-21 00:26:26 +0100

    Update RpcBaseCommand.cs

--- cli/Commands/Rpc/RpcBaseCommand.cs
+++ cli/Commands/Rpc/RpcBaseCommand.cs
@@ -5,32 +5,43 @@
 //
 // You should have received a copy of the license along with this
 // work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+// Improved by ChatGPT
 
 using System;
 using System.Threading;
 using BAMWallet.HD;
-using Cli.Commands.Common;
 
 namespace Cli.Commands.Rpc
 {
-    [CommandDescriptor("", "")]
-    public abstract class RpcBaseCommand : Command
+    public abstract class RpcCommandBase : Command
     {
-        protected AutoResetEvent _cmdFinishedEvent;
-        protected Session _session;
-        public RpcBaseCommand(IServiceProvider serviceProvider, ref AutoResetEvent cmdFinishedEvent, Session session)
-            : base(typeof(RpcBaseCommand), serviceProvider)
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        private readonly Session _session;
+
+        public RpcCommandBase(IServiceProvider serviceProvider, Session session)
+            : base(typeof(RpcCommandBase), serviceProvider)
         {
-            _cmdFinishedEvent = cmdFinishedEvent;
+            _cancellationTokenSource = new CancellationTokenSource();
             _session = session;
             Result = new Tuple<object, string>(null, "Command not executed.");
         }
 
         public Tuple<object, string> Result { get; protected set; }
 
-        public void Wait()
+        public void Cancel()
         {
-            _cmdFinishedEvent.WaitOne();
+            _cancellationTokenSource.Cancel();
+        }
+
+        public Session Session
+        {
+            get { return _session; }
+            set { /* Add any necessary validation here */ _session = value; }
+        }
+
+        public CancellationToken CancellationToken
+        {
+            get { return _cancellationTokenSource.Token; }
         }
     }
 }
