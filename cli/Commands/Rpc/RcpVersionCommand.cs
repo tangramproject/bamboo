2917a3020ac93a071c0d08f7e66eb6ae81fbf87b
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-21 00:15:37 +0100

    Update RcpVersionCommand.cs

--- cli/Commands/Rpc/RcpVersionCommand.cs
+++ cli/Commands/Rpc/RcpVersionCommand.cs
@@ -1,37 +1,35 @@
+//Improved by ChatGPT
+
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 using BAMWallet.HD;
-using Cli.Commands.Rpc;
 
-namespace CLi.Commands.Rpc;
-
-public class RcpVersionCommand : RpcBaseCommand
+namespace Cli.Commands.Rpc
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="serviceProvider"></param>
-    /// <param name="cmdFinishedEvent"></param>
-    public RcpVersionCommand(IServiceProvider serviceProvider, ref AutoResetEvent cmdFinishedEvent) :
-        base(serviceProvider, ref cmdFinishedEvent, null)
+    public class RpcVersionCommand : RpcBaseCommand
     {
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="activeSession"></param>
-    /// <returns></returns>
-    public override Task Execute(Session activeSession = null)
-    {
-        Result = new Tuple<object, string>(new
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RpcVersionCommand"/> class.
+        /// </summary>
+        /// <param name="serviceProvider">The service provider.</param>
+        /// <param name="cmdFinishedEvent">The command finished event.</param>
+        public RpcVersionCommand(IServiceProvider serviceProvider, ref AutoResetEvent cmdFinishedEvent) :
+            base(serviceProvider, ref cmdFinishedEvent, null)
         {
-            Version = BAMWallet.Helper.Util.GetAssemblyVersion()
-        }, string.Empty);
+        }
 
-        _cmdFinishedEvent.Set();
-
-        return Task.CompletedTask;
+        /// <summary>
+        /// Executes the RPC version command asynchronously.
+        /// </summary>
+        /// <param name="activeSession">The active session.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        public override async Task ExecuteAsync(Session activeSession = null)
+        {
+            var version = BAMWallet.Helper.Util.GetAssemblyVersion();
+            Result = new Tuple<object, string>(new { Version = version }, string.Empty);
+            _cmdFinishedEvent.Set();
+            await Task.CompletedTask;
+        }
     }
 }
