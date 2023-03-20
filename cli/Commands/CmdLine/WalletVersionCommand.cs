efe124318ecadfb33d5cd880f29d67a0a0a99624
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-20 23:07:19 +0100

    Update WalletVersionCommand.cs

--- cli/Commands/CmdLine/WalletVersionCommand.cs
+++ cli/Commands/CmdLine/WalletVersionCommand.cs
@@ -5,12 +5,10 @@
 //
 // You should have received a copy of the license along with this
 // work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+// Improved by ChatGPT
 
 using System;
 using System.Threading.Tasks;
-using Cli.Commands.Common;
-using BAMWallet.HD;
-
 using McMaster.Extensions.CommandLineUtils;
 
 namespace Cli.Commands.CmdLine
@@ -18,15 +16,26 @@ namespace Cli.Commands.CmdLine
     [CommandDescriptor("version", "Running version")]
     public class WalletVersionCommand : Command
     {
-        public WalletVersionCommand(IServiceProvider serviceProvider)
+        private readonly IConsole _console;
+
+        public WalletVersionCommand(IServiceProvider serviceProvider, IConsole console)
             : base(typeof(WalletVersionCommand), serviceProvider)
         {
+            _console = console;
         }
 
         public override Task Execute(Session activeSession = null)
         {
-            _console.WriteLine($"{BAMWallet.Helper.Util.GetAssemblyVersion()}");
-            return Task.CompletedTask;
+            try
+            {
+                _console.WriteLine($"{BAMWallet.Helper.Util.GetAssemblyVersion()}");
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                _console.Error.WriteLine($"An error occurred: {ex.Message}");
+                return Task.FromException(ex);
+            }
         }
     }
 }
