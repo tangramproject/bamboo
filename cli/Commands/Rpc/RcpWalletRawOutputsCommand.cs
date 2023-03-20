1dd1d568bd123de5f7d3c9492049062d5f22eb9e
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-21 00:17:07 +0100

    Update RcpWalletRawOutputsCommand.cs

--- cli/Commands/Rpc/RcpWalletRawOutputsCommand.cs
+++ cli/Commands/Rpc/RcpWalletRawOutputsCommand.cs
@@ -5,6 +5,7 @@
 //
 // You should have received a copy of the license along with this
 // work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+// Imprved by ChatGPT
 
 using System;
 using System.Linq;
@@ -15,52 +16,43 @@ using BAMWallet.HD;
 using BAMWallet.Model;
 using Cli.Commands.Rpc;
 
-namespace CLi.Commands.Rpc;
-
-public class RcpWalletRawOutputsCommand : RpcBaseCommand
+namespace Cli.Commands.Rpc
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="serviceProvider"></param>
-    /// <param name="cmdFinishedEvent"></param>
-    /// <param name="session"></param>
-    public RcpWalletRawOutputsCommand(IServiceProvider serviceProvider, ref AutoResetEvent cmdFinishedEvent, Session session) : base(serviceProvider, ref cmdFinishedEvent, session)
+    public class RpcWalletRawOutputsCommand : RpcBaseCommand
     {
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="activeSession"></param>
-    public override async Task Execute(Session activeSession = null)
-    {
-        try
+        public RpcWalletRawOutputsCommand(IServiceProvider serviceProvider, ref AutoResetEvent cmdFinishedEvent, Session session)
+            : base(serviceProvider, ref cmdFinishedEvent, session)
         {
-            await _commandReceiver.SyncWallet(_session);
-            var recover = _commandReceiver.RecoverTransactions(_session, 0);
-            var balances = _commandReceiver.GetBalances(_session);
-            var listOutputs = balances.Select(balance => new OutputText
-            {
-                C = balance.Commitment.C.ByteToHex(),
-                E = balance.Commitment.E.ByteToHex(),
-                N = balance.Commitment.N.ByteToHex(),
-                T = (sbyte)balance.Commitment.T
-            }).ToList();
-
-            Result = new Tuple<object, string>(new
-            {
-                Outputs = listOutputs
-            }, string.Empty);
-
         }
-        catch (Exception ex)
+
+        public override async Task Run(Session activeSession = null)
         {
-            Result = new Tuple<object, string>(null, ex.Message);
-        }
-        finally
-        {
-            _cmdFinishedEvent.Set();
+            try
+            {
+                await _commandReceiver.SyncWallet(_session);
+                var balances = _commandReceiver.GetBalances(_session);
+
+                var listOutputs = balances.Select(balance => new OutputText
+                {
+                    C = balance.Commitment.C.ByteToHex(),
+                    E = balance.Commitment.E.ByteToHex(),
+                    N = balance.Commitment.N.ByteToHex(),
+                    T = (sbyte)balance.Commitment.T
+                }).ToList();
+
+                Result = (new
+                {
+                    Outputs = listOutputs
+                }, string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Result = (null, ex.Message);
+            }
+            finally
+            {
+                _cmdFinishedEvent.Set();
+            }
         }
     }
 }
