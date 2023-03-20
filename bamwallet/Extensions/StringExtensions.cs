5adbaccc147bbb61b67df3b3a2e736acf94f0601
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-21 00:55:32 +0100

    Update StringExtensions.cs

--- bamwallet/Extensions/StringExtensions.cs
+++ bamwallet/Extensions/StringExtensions.cs
@@ -1,12 +1,22 @@
-﻿using System;
+// Improved by ChatGPT
+
+using System;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security;
 
 namespace BAMWallet.Extensions
 {
+    /// <summary>
+    /// Provides extension methods for strings.
+    /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Converts the string to a secure string.
+        /// </summary>
+        /// <param name="value">The string to convert.</param>
+        /// <returns>The secure string.</returns>
         public static SecureString ToSecureString(this string value)
         {
             var secureString = new SecureString();
@@ -15,36 +25,59 @@ namespace BAMWallet.Extensions
             return secureString;
         }
 
+        /// <summary>
+        /// Converts the hexadecimal string to a byte array.
+        /// </summary>
+        /// <param name="hex">The hexadecimal string to convert.</param>
+        /// <returns>The byte array.</returns>
         public static byte[] HexToByte(this string hex)
         {
             return Convert.FromHexString(hex);
         }
 
+        /// <summary>
+        /// Converts the generic type to a hexadecimal string and then to a byte array.
+        /// </summary>
+        /// <typeparam name="T">The type to convert.</typeparam>
+        /// <param name="hex">The type to convert.</param>
+        /// <returns>The byte array.</returns>
         public static byte[] HexToByte<T>(this T hex)
         {
             return Convert.FromHexString(hex.ToString()!);
         }
 
+        /// <summary>
+        /// Overwrites the string with null characters.
+        /// </summary>
+        /// <param name="value">The string to zero.</param>
         public static void ZeroString(this string value)
         {
             var handle = GCHandle.Alloc(value, GCHandleType.Pinned);
             unsafe
             {
                 var pValue = (char*)handle.AddrOfPinnedObject();
-                for (int index = 0; index < value.Length; index++)
-                {
-                    pValue[index] = char.MinValue;
-                }
+                var span = new ReadOnlySpan<char>(pValue, value.Length);
+                span.Fill(char.MinValue);
             }
 
             handle.Free();
         }
 
+        /// <summary>
+        /// Converts the string to a decimal.
+        /// </summary>
+        /// <param name="value">The string to convert.</param>
+        /// <returns>The decimal.</returns>
         public static decimal ToDecimal(this string value)
         {
             return Convert.ToDecimal(value);
         }
 
+        /// <summary>
+        /// Converts the string to a 64-bit integer.
+        /// </summary>
+        /// <param name="value">The string to convert.</param>
+        /// <returns>The 64-bit integer.</returns>
         public static long ToInt64(this string value)
         {
             return Convert.ToInt64(value);
