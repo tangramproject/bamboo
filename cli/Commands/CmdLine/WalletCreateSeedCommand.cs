d0eedbeff1ef8cc327d41a6d6f877ec44d8f5032
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-20 23:34:02 +0100

    Update WalletCreateSeedCommand.cs

--- cli/Commands/CmdLine/WalletCreateSeedCommand.cs
+++ cli/Commands/CmdLine/WalletCreateSeedCommand.cs
@@ -5,6 +5,7 @@
 //
 // You should have received a copy of the license along with this
 // work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+// Improved by ChatGPT
 
 using System;
 using System.Threading.Tasks;
@@ -12,45 +13,39 @@ using BAMWallet.HD;
 using Cli.Commands.Common;
 using McMaster.Extensions.CommandLineUtils;
 using NBitcoin;
+
 namespace Cli.Commands.CmdLine
 {
     [CommandDescriptor("seed", "Create new seed and passphrase")]
     class WalletCreateMnemonicCommand : Command
     {
-        public WalletCreateMnemonicCommand(IServiceProvider serviceProvider)
-            : base(typeof(WalletCreateMnemonicCommand), serviceProvider, true)
+        private readonly ICommandReceiver _commandReceiver;
+        private readonly IConsole _console;
+
+        public WalletCreateMnemonicCommand(ICommandReceiver commandReceiver, IConsole console)
         {
+            _commandReceiver = commandReceiver;
+            _console = console;
         }
 
-        public override Task Execute(Session activeSession = null)
+        public override async Task Execute(Session activeSession = null)
         {
             _console.ForegroundColor = ConsoleColor.Magenta;
             _console.WriteLine("\nSeed phrase\n");
 
-            Options(out WordCount wCount, 3);
+            var wCount = await Options(3);
 
-            var seed = _commandReceiver.CreateSeed(wCount);
-            _console.ForegroundColor = ConsoleColor.Magenta;
-            _console.WriteLine("");
-            _console.WriteLine("Passphrase");
+            var mnemonic = new Mnemonic(Wordlist.English, wCount);
+            var seed = mnemonic.DeriveSeed();
+            var passphrase = await Options(1);
 
-            Options(out wCount, 1);
-            var passphrase = _commandReceiver.CreateSeed(wCount);
-
-            _console.WriteLine("Seed phrase: " + string.Join(" ", seed));
-            _console.WriteLine("Passphrase:  " + string.Join(" ", passphrase));
+            _console.WriteLine("Seed phrase: " + mnemonic);
+            _console.WriteLine("Passphrase:  " + passphrase);
 
             _console.ForegroundColor = ConsoleColor.White;
-
-            return Task.CompletedTask;
         }
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="wCount"></param>
-        /// <param name="defaultAnswer"></param>
-        private void Options(out WordCount wCount, int defaultAnswer)
+        private async Task<WordCount> Options(int defaultAnswer)
         {
             _console.ForegroundColor = ConsoleColor.Magenta;
 
@@ -59,25 +54,11 @@ namespace Cli.Commands.CmdLine
             _console.WriteLine("18    [2]");
             _console.WriteLine("24    [3]\n");
 
-            var wordCount = Prompt.GetInt("Select word count:", defaultAnswer, ConsoleColor.Yellow);
+            var wordCount = await Prompt.GetIntAsync("Select word count:", defaultAnswer, ConsoleColor.Yellow);
 
             _console.WriteLine("");
 
-            switch (wordCount)
-            {
-                case 1:
-                    wordCount = 12;
-                    break;
-                case 2:
-                    wordCount = 18;
-                    break;
-                case 3:
-                    wordCount = 24;
-                    break;
-                default:
-                    break;
-            }
-            wCount = (WordCount)wordCount;
+            return (WordCount)wordCount;
         }
     }
 }
