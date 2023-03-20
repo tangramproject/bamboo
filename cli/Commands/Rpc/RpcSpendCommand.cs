899fabbb8a9f885afa3de53e5db87fc37dbaefc7
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-21 00:32:43 +0100

    Update RpcSpendCommand.cs

--- cli/Commands/Rpc/RpcSpendCommand.cs
+++ cli/Commands/Rpc/RpcSpendCommand.cs
@@ -5,66 +5,66 @@
 //
 // You should have received a copy of the license along with this
 // work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+// Improved by ChatGPT
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using BAMWallet.Extensions;
 using BAMWallet.HD;
 using BAMWallet.Model;
 
-
 namespace Cli.Commands.Rpc
 {
     class RpcSpendCommand : RpcBaseCommand
     {
-        private WalletTransaction _transaction;
-        public RpcSpendCommand(ref WalletTransaction tx, IServiceProvider serviceProvider, ref AutoResetEvent cmdFinishedEvent, Session session)
+        private readonly WalletTransaction _transaction;
+
+        public RpcSpendCommand(ref WalletTransaction transaction, IServiceProvider serviceProvider, ref AutoResetEvent cmdFinishedEvent, Session session)
             : base(serviceProvider, ref cmdFinishedEvent, session)
         {
-            _transaction = tx;
+            _transaction = transaction;
         }
 
-        public override Task Execute(Session activeSession = null)
+        public override async Task Execute(Session activeSession = null)
         {
             try
             {
-                var canSpend = _commandReceiver.GetSpending(_session, _transaction);
-                if (!canSpend.Success)
+                using var canSpendResult = _commandReceiver.GetSpending(_session, _transaction);
+                if (!canSpendResult.Success)
                 {
-                    throw new Exception(canSpend.NonSuccessMessage);
+                    throw new Exception(canSpendResult.NonSuccessMessage);
                 }
-                var createPaymentResult = _commandReceiver.CreateTransaction(_session, ref _transaction);
-                if (createPaymentResult.Item1 is null)
+
+                using var createTransactionResult = _commandReceiver.CreateTransaction(_session, ref _transaction);
+                if (createTransactionResult.Item1 is null)
                 {
-                    Result = new Tuple<object, string>(null, createPaymentResult.Item2);
+                    Result = new Tuple<object, string>(null, createTransactionResult.Item2);
+                    return;
                 }
-                else
+
+                using var sendTransactionResult = _commandReceiver.SendTransaction(_session, ref _transaction);
+                if (sendTransactionResult.Item1 is null)
                 {
-                    var send = _commandReceiver.SendTransaction(_session, ref _transaction);
-                    if (send.Item1 is null)
-                    {
-                        Result = new Tuple<object, string>(null, send.Item2);
-                    }
-                    else
-                    {
-                        var history = _commandReceiver.History(_session);
-                        if (history.Item1 is null)
-                        {
-                            Result = new Tuple<object, string>(null, history.Item2);
-                        }
-                        else
-                        {
-                            Result = new Tuple<object, string>(new
-                            {
-                                balance = $"{(history.Item1 as IList<BalanceSheet>).Last().Balance}",
-                                paymentId = _transaction.Transaction.TxnId.ByteToHex()
-                            }, String.Empty);
-                        }
-                    }
+                    Result = new Tuple<object, string>(null, sendTransactionResult.Item2);
+                    return;
                 }
+
+                using var historyResult = _commandReceiver.History(_session);
+                if (historyResult.Item1 is null)
+                {
+                    Result = new Tuple<object, string>(null, historyResult.Item2);
+                    return;
+                }
+
+                var balance = (historyResult.Item1 as IList<BalanceSheet>).Last().Balance;
+                var paymentId = _transaction.Transaction.TxnId.ByteToHex();
+                Result = new Tuple<object, string>(new
+                {
+                    balance = $"{balance}",
+                    paymentId
+                }, String.Empty);
             }
             catch (Exception ex)
             {
@@ -75,7 +75,7 @@ namespace Cli.Commands.Rpc
                 _cmdFinishedEvent.Set();
             }
 
-            return Task.CompletedTask;
+            await Task.CompletedTask;
         }
     }
 }
