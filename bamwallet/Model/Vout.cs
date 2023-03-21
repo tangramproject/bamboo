bb937f12824d345db428b7753d77ffd91297f99e
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-21 13:24:58 +0100

    Update Vout.cs

--- bamwallet/Model/Vout.cs
+++ bamwallet/Model/Vout.cs
@@ -1,4 +1,4 @@
-﻿// CypherNetwork BAMWallet by Matthew Hellyer is licensed under CC BY-NC-ND 4.0.
+// CypherNetwork BAMWallet by Matthew Hellyer is licensed under CC BY-NC-ND 4.0.
 // To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-nd/4.0
 
 using System;
@@ -13,35 +13,30 @@ namespace BAMWallet.Model
     [MessagePackObject]
     public record Vout
     {
-        [Key(0)] public ulong A { get; set; }
+        [Key(0)] public ulong Amount { get; set; }
         [Key(1)] public byte[] C { get; set; }
         [Key(2)] public byte[] E { get; set; }
-        [Key(3)] public long L { get; set; }
+        [Key(3)] public long LockTime { get; set; }
         [Key(4)] public byte[] N { get; set; }
         [Key(5)] public byte[] P { get; set; }
-        [Key(6)] public byte[] S { get; set; }
-        [Key(7)] public CoinType T { get; set; }
-        [Key(8)] public byte[] D { get; set; }
+        [Key(6)] public byte[] Script { get; set; }
+        [Key(7)] public CoinType Type { get; set; }
+        [Key(8)] public byte[] Data { get; set; }
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <returns></returns>
         public bool IsLockedOrInvalid()
         {
-            if (T != CoinType.Coinbase) return false;
+            if (Type != CoinType.Coinbase) return false;
 
-            var lockTime = new LockTime(Util.UnixTimeToDateTime(L));
-            var script = S.FromBytes();
-            var sc1 = new Script(Op.GetPushOp(lockTime.Value), OpcodeType.OP_CHECKLOCKTIMEVERIFY);
-            var sc2 = new Script(script);
+            var lockTime = new LockTime(Util.UnixTimeToDateTime(LockTime));
+            var sc1 = new Script($"{lockTime.Value} OP_CHECKLOCKTIMEVERIFY");
+            var sc2 = new Script(Script);
             if (!sc1.ToString().Equals(sc2.ToString()))
             {
                 return true;
             }
 
             var tx = Network.Main.CreateTransaction();
-            tx.Outputs.Add(new TxOut { ScriptPubKey = new Script(script) });
+            tx.Outputs.Add(new TxOut { ScriptPubKey = new Script(Script) });
             var spending = Network.Main.CreateTransaction();
             spending.LockTime = new LockTime(DateTimeOffset.UtcNow);
             spending.Inputs.Add(new TxIn(tx.Outputs.AsCoins().First().Outpoint, new Script()));
