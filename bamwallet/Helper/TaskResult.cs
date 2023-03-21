51bcbf504d4c54796240bbbe217c9e59157cc79b
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-21 13:49:39 +0100

    Update TaskResult.cs

--- bamwallet/Helper/TaskResult.cs
+++ bamwallet/Helper/TaskResult.cs
@@ -1,59 +1,37 @@
-﻿using System;
+using System;
 
 namespace BAMWallet.Helper
 {
     public class TaskResult<T>
     {
-        private TaskResult()
-        {
-
-        }
+        private TaskResult() { }
 
         public bool Success { get; private set; }
         public T Result { get; private set; }
         public dynamic NonSuccessMessage { get; private set; }
         public Exception Exception { get; private set; }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="result"></param>
-        /// <returns></returns>
         public static TaskResult<T> CreateSuccess(T result)
         {
             return new TaskResult<T> { Success = result != null, Result = result };
         }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="successMessage"></param>
-        /// <returns></returns>
+
         public static TaskResult<T> CreateSuccess(dynamic successMessage)
         {
-            return new TaskResult<T> { Success = successMessage != null, Result = successMessage };
+            return new TaskResult<T> { Success = !string.IsNullOrEmpty(successMessage), Result = successMessage };
         }
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="nonSuccessMessage"></param>
-        /// <returns></returns>
         public static TaskResult<T> CreateFailure(dynamic nonSuccessMessage)
         {
             return new TaskResult<T> { Success = false, Result = default, NonSuccessMessage = nonSuccessMessage };
         }
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="ex"></param>
-        /// <returns></returns>
         public static TaskResult<T> CreateFailure(Exception ex)
         {
             return new TaskResult<T>
             {
                 Success = false,
-                NonSuccessMessage = $"{ex.Message}{Environment.NewLine}{ex.StackTrace ?? null}",
+                NonSuccessMessage = $"{ex.Message}{Environment.NewLine}{ex.StackTrace ?? string.Empty}",
                 Exception = ex,
                 Result = default,
             };
