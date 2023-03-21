1fd31c91f2ea50fee6c54029892f924f0ddb2f7b
Raizen9 <43849097+Raizen9@users.noreply.github.com>
2023-03-21 13:15:50 +0100

    Update AddressBook.cs

--- bamwallet/Model/AddressBook.cs
+++ bamwallet/Model/AddressBook.cs
@@ -1,14 +1,15 @@
 using System;
 using LiteDB;
 
-namespace BAMWallet.Model;
-
-public class AddressBook
+namespace BAMWallet.Model
 {
-    [BsonId]
-    public Guid Id { get; set; }
-    public DateTime Created { get; set; }
-    public string RecipientAddress { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public class AddressBook
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public string RecipientAddress { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+    }
 }
